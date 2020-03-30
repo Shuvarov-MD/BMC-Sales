0658e4f907fdb4c4551d947e7bf52fade98460c7
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-31 01:58:30 +0300

    feat: add sales, timer, services, popular, mobile-app sections

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,15 @@ $(document).ready(function () {
       nextEl: '.hero__swiper-button-next',
       prevEl: '.hero__swiper-button-prev',
     },
-  })
+  });
+
+  var serviceSwiper = new Swiper('.services__swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+      nextEl: '.services__swiper-button-next',
+      prevEl: '.services__swiper-button-prev',
+    },
+  });
 });
